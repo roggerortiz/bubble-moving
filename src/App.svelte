6844f43566e6321e0c91ab72bc67c94b6016e4a3
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2025-06-21 23:41:14 -0500

    move commands to input

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
   import { Canvas } from './classes/Canvas'
   import { MINUS_KEY, PLUS_KEY, SPACE_BAR_KEY, X_KEY, Z_KEY } from './helpers/constants'
 
   let canvas: Canvas = $state(new Canvas())
   let pause: boolean = $state(false)
-  let inputEl: HTMLInputElement
-
-  onMount(() => inputEl && inputEl.focus())
 
   const onKeyDown = ({ key }: KeyboardEvent) => {
     if (key === Z_KEY) {
@@ -43,18 +39,13 @@
   }
 </script>
 
-<main class="w-full md:w-10/12 xl:w-8/12 bg-zinc-950 border border-gray-700 rounded overflow-hidden">
+<main class="w-full md:w-10/12 xl:w-8/12 bg-zinc-950 border border-gray-700 rounded overflow-hidden mx-4 md:m-0">
   <canvas class="w-full"></canvas>
-  <div class="absolute w-full bottom-0 sm:relative bg-zinc-950 border-t border-gray-700 -mx-4 sm:m-0">
-    <div class="flex flex-wrap gap-x-2 sm:gap-x-4 text-gray-400 text-sm px-4 py-1 sm:py-2">
-      <span>Pause : [ ]</span>
-      <span>Speed : [+] / [-]</span>
-      <span>Bubble : [N] / [X]</span>
-      <input
-        class="hidden"
-        bind:this={inputEl}
-      />
-    </div>
+  <div class="flex flex-wrap gap-x-2 bg-zinc-950 border-t border-gray-700">
+    <input
+      class="w-full outline-0 text-gray-400 text-sm px-4 py-2 caret-transparent"
+      value="Pause : [ ]  Speed : [+] / [-]  Bubble : [N] / [X]"
+    />
   </div>
 </main>
 
